27b9425886bf9a8fa466a4b397c80175e73e2a9e
plightick <japanas2007@gmail.com>
2025-10-16 19:15:08 +0300

    auto

--- lab3/src/operations_person.cc
+++ lab3/src/operations_person.cc
@@ -76,7 +76,7 @@ void inputTeacher(Teacher*& teacher) {
     teacher->setSpecialty(specialty);
 
     cout << "Количество публикаций: "; publicationCount = checkInt();
-    string* publications = new string[publicationCount];
+    auto* publications = new string[publicationCount];
     for (int i = 0; i < publicationCount; i++) {
         cout << "Публикация " << (i+1) << ": "; cin >> publications[i];
     }
@@ -115,7 +115,7 @@ void inputCommissionMember(CommissionMember*& member) {
     member->setCertificateNumber(certificateNumber);
 
     cout << "Количество записей автобиографии: "; autobiographyCount = checkInt();
-    string* autobiography = new string[autobiographyCount];
+    auto* autobiography = new string[autobiographyCount];
     for (int i = 0; i < autobiographyCount; i++) {
         cout << "Запись " << (i+1) << ": "; cin >> autobiography[i];
     }
@@ -153,7 +153,7 @@ void inputTeacherCommissionMember(TeacherCommissionMember*& tcm) {
     tcm->setSpecialty(specialty);
 
     cout << "Количество публикаций: "; publicationCount = checkInt();
-    string* publications = new string[publicationCount];
+    auto* publications = new string[publicationCount];
     for (int i = 0; i < publicationCount; i++) {
         cout << "Публикация " << (i+1) << ": "; cin >> publications[i];
     }
@@ -174,7 +174,7 @@ void inputTeacherCommissionMember(TeacherCommissionMember*& tcm) {
     tcm->setCertificateNumber(certificateNumber);
 
     cout << "Количество записей автобиографии: "; autobiographyCount = checkInt();
-    string* autobiography = new string[autobiographyCount];
+    auto* autobiography = new string[autobiographyCount];
     for (int i = 0; i < autobiographyCount; i++) {
         cout << "Запись " << (i+1) << ": "; cin >> autobiography[i];
     }
@@ -183,7 +183,7 @@ void inputTeacherCommissionMember(TeacherCommissionMember*& tcm) {
 
     int commissionWorkCount;
     cout << "Количество работ в комиссии: "; commissionWorkCount = checkInt();
-    string* works = new string[commissionWorkCount];
+    auto* works = new string[commissionWorkCount];
     for (int i = 0; i < commissionWorkCount; i++) {
         cout << "Работа " << (i+1) << ": "; cin >> works[i];
     }
